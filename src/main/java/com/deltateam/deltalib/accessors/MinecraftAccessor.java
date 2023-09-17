0a447eb2289a79b96f59b3a8a9616317ab5bbb4a
LOLAtom <castroarnaud@gmail.com>
2023-09-17 13:19:24 +0200

    MinecraftAccessor Multiple access added

--- src/main/java/com/deltateam/deltalib/accessors/MinecraftAccessor.java
+++ src/main/java/com/deltateam/deltalib/accessors/MinecraftAccessor.java
@@ -1,8 +1,30 @@
 package com.deltateam.deltalib.accessors;
 
+import com.mojang.datafixers.DataFixer;
+import net.minecraft.client.Timer;
+import net.minecraft.client.tutorial.Tutorial;
+import net.minecraft.server.level.progress.StoringChunkProgressListener;
+import net.minecraft.util.FrameTimer;
+import net.minecraft.util.profiling.ProfilerFiller;
+
+import java.io.File;
+import java.util.concurrent.atomic.AtomicReference;
+
 public interface MinecraftAccessor {
 
     float getPausePartialTicks();
+
+    Timer getTimer();
+
+    DataFixer getFixerUpper();
+
+    ProfilerFiller getProfiler();
+
+    FrameTimer getFrameTimer();
+
+    AtomicReference<StoringChunkProgressListener> getStoringChunkProgressListener();
+
+    void takePanoramicScreenshot(File file, int Width, int Height);
 
     void openChatScreenAction(String defaultText);
 }
--- src/main/java/com/deltateam/deltalib/mixins/client/MinecraftClassMixin.java
+++ src/main/java/com/deltateam/deltalib/mixins/client/MinecraftClassMixin.java
@@ -1,21 +1,75 @@
 package com.deltateam.deltalib.mixins.client;
 
 import com.deltateam.deltalib.accessors.MinecraftAccessor;
+import com.mojang.datafixers.DataFixer;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.Timer;
+import net.minecraft.client.tutorial.Tutorial;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.progress.StoringChunkProgressListener;
+import net.minecraft.util.FrameTimer;
+import net.minecraft.util.profiling.ProfilerFiller;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
+import java.io.File;
+import java.util.concurrent.atomic.AtomicReference;
+
 @Mixin(Minecraft.class)
 public abstract class MinecraftClassMixin implements MinecraftAccessor {
-
 
     @Shadow private float pausePartialTick;
 
     @Shadow protected abstract void openChatScreen(String pDefaultText);
 
+    @Shadow @Final private Tutorial tutorial;
+
+    @Shadow @Final private Timer timer;
+
+    @Shadow @Final public FrameTimer frameTimer;
+
+    @Shadow @Final private DataFixer fixerUpper;
+
+    @Shadow private ProfilerFiller profiler;
+
+    @Shadow public abstract Component grabPanoramixScreenshot(File p_167900_, int pWidth, int pHeight);
+
+    @Shadow @Final private AtomicReference<StoringChunkProgressListener> progressListener;
+
     @Override
     public float getPausePartialTicks() {
         return pausePartialTick;
+    }
+
+    @Override
+    public Timer getTimer() {
+        return timer;
+    }
+
+    @Override
+    public FrameTimer getFrameTimer() {
+        return frameTimer;
+    }
+
+    @Override
+    public DataFixer getFixerUpper() {
+        return fixerUpper;
+    }
+
+    @Override
+    public ProfilerFiller getProfiler() {
+        return profiler;
+    }
+
+    @Override
+    public AtomicReference<StoringChunkProgressListener> getStoringChunkProgressListener() {
+        return progressListener;
+    }
+
+    @Override
+    public void takePanoramicScreenshot(File file, int width, int height) {
+        grabPanoramixScreenshot(file,width,height);
     }
 
     @Override
